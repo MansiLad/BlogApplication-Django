8478d8b457f0035bf05ed0fec3754bd5f1611a62
MANSI MISTRY <65220621+MansiLad@users.noreply.github.com>
2021-01-19 20:56:04 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'DRIVER': 'ODBC Driver 17 for SQL Server'
     }
 }
 
